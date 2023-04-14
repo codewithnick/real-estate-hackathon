e1ca14bb9794f148f741c7513d87ae183eab1e55
hiteshkumar8642 <hiteshkumar8642@gmail.com>
2023-04-14 22:39:46 +0530

    property deleted

--- property/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Property(models.Models):
-    title=models.TextField()
-    description=models.TextField()
-    price=models.TextField()
